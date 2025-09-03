9cf4bc19245529c5eec041272f9ab9321f5ff0f8
Kevin Gabriel <161509299+KevinSouza17@users.noreply.github.com>
2025-09-03 16:57:42 -0300

    Add files via upload

--- primeiro-teste/src/components/Formulario.js
+++ primeiro-teste/src/components/Formulario.js
@@ -1,15 +1,28 @@
+import { useState } from "react";
+
 function Formulario(){
 
     function cadastrarUsuario(e){
         e.preventDefault();
+        console.log(nome)
+        console.log(`Usuário ${nome} cadastrado com a senha: ${senha}`)
         console.log('cadastrou o usuario');
     }
+
+    const [nome, setNome] = useState('Anderson')
+    const [senha, setSenha] = useState()
+
     return(
         <div>
            <h1>Cadastro</h1>
            <form onSubmit={cadastrarUsuario}>
                <div>
-                   <input type="text" placeholder="Digite seu nome"/>
+                <label htmlFor="nome">Nome:</label>
+                   <input type="text" id="nome" name="nome" value={nome} onChange={(e)=> setNome(e.target.value)} placeholder="Digite seu nome"/>
+                </div>
+                 <div>
+                    <label htmlFor="senha">Senha:</label>
+                   <input type="password" id="senha" name="senha" onChange={(e)=> setNome(e.target.value)} placeholder="Digite sua senha"/>
                 </div>
                 <div>
                     <input type="submit" placeholder="Cadastrar"/>
